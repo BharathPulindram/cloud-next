7d11d83484abd818a1f2d78134a23e98f9650c42
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-04-29 12:28:55 -0500

    using styled components as much as possible

--- Components/Sidebar.jsx
+++ Components/Sidebar.jsx
@@ -1,12 +1,28 @@
 import { Avatar } from "@mui/material";
 import styled from "styled-components";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { IconButton } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 function Sidebar() {
   return (
     <Container>
       <Header>
         <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
       </Header>
+      <Search>
+        <SearchOutlinedIcon />
+      </Search>
     </Container>
   );
 }
@@ -15,6 +31,26 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
 
-const UserAvatar = styled(Avatar)``;
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
+
+const Search = styled.div``;
